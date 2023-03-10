cb5630bb22471d1ec2ca408581d55733d5c1843a
annettalekto <annanesterova248@gmail.com>
2023-03-10 21:02:08 +0300

    +list

--- widgets/main.go
+++ widgets/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -10,7 +12,57 @@ import (
 func main() {
 	a := app.New()
 	w := a.NewWindow("Fyne test")
-	w.Resize(fyne.NewSize(800, 600))
+	w.Resize(fyne.NewSize(600, 400))
+	w.CenterOnScreen()
+	w.SetMaster()
+
+	var (
+		LabelWidgets  = "Label метки"
+		ButtonWidgets = "Кнопки"
+		SelectWidgets = "Выбор"
+	)
+
+	// лист
+	dataForList := []string{LabelWidgets, SelectWidgets, ButtonWidgets}
+	list := widget.NewList(
+		func() int {
+			return len(dataForList)
+		},
+		func() fyne.CanvasObject {
+			var style fyne.TextStyle
+			style.Monospace = true
+			temp := widget.NewLabelWithStyle("temp", fyne.TextAlignLeading, style)
+			return temp
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			if i < len(dataForList) {
+				o.(*widget.Label).SetText(dataForList[i])
+			}
+		})
+
+	l1 := widget.NewLabel(" ")
+	list.OnSelected = func(id widget.ListItemID) {
+		wt := dataForList[id]
+		l1.SetText(fmt.Sprintf("Выбран элемент «%s»", wt))
+		if wt == LabelWidgets {
+			LabelWidgetsForm()
+		}
+	}
+	l2 := widget.NewLabel(" ")
+	list.OnUnselected = func(id widget.ListItemID) {
+		wt := dataForList[id]
+		l2.SetText(fmt.Sprintf("Снято выделение с элемента «%s»", wt))
+	}
+
+	box := container.NewVBox(l1, l2)
+	w.SetContent(container.NewHSplit(list, box))
+	w.ShowAndRun()
+}
+
+func LabelWidgetsForm() {
+	w := fyne.CurrentApp().NewWindow("Fyne Label")
+	w.CenterOnScreen()
+	w.Resize(fyne.NewSize(400, 400))
 
 	// варианты label
 	l1 := widget.NewLabel("Test label: Italic style")
@@ -21,12 +73,12 @@ func main() {
 	l3.TextStyle.Bold = true
 	l4 := widget.NewLabel("Test label: Symbol style")
 	l4.TextStyle.Symbol = true
-	l5 := widget.NewLabel("Test label: TabWidth=1")
+	l5 := widget.NewLabel("Test label: TabWidth=1") // todo: tab?
 	l5.TextStyle.TabWidth = 1
 	l6 := widget.NewLabel("Test label: TabWidth=5")
 	l6.TextStyle.TabWidth = 5
 
 	labelBox := container.NewVBox(l1, l2, l3, l4, l5, l6)
 	w.SetContent(labelBox)
-	w.ShowAndRun()
+	w.Show()
 }
