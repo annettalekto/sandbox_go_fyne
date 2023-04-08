3b20d91dae48b572878e743160ec902691dbf013
annettalekto <annanesterova248@gmail.com>
2023-04-09 00:27:30 +0300

    ent cmt

--- entry/main.go
+++ entry/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -17,16 +19,20 @@ func main() {
 	// in1.Wrapping = fyne.TextWrapOff // окно проги разъезжается при заполнени виджета. тупо выглядит
 	// in1.Wrapping = fyne.TextWrapBreak // для мультистрочного виджета (тут просто прокрутка)
 	// in1.Wrapping = fyne.TextWrapWord // для мультистрочного виджета (тут просто прокрутка)
+	in1.OnChanged = func(s string) { fmt.Println(s) }
+	in1.TextStyle.Bold = true
 
 	in2 := widget.NewMultiLineEntry() // при создании элемента Wrapping = TextTruncate
 	// in2.Wrapping = fyne.TextTruncate  // при заполнении виджита появляются вертикальная и горизонтальная полосы прокрутки
 	// in2.Wrapping = fyne.TextWrapOff // окно проги разъезжается
 	// in2.Wrapping = fyne.TextWrapBreak // перенос того что неуместилось на другую строку - прям по стредине слова (горизонтальной полосы прокрутки нет)
 	in2.Wrapping = fyne.TextWrapWord // самый удобный для редактора: перенос по словам (горизонтальной полосы прокрутки нет)
+	in2.TextStyle.Italic = true
 
 	// расширение возможностей базового типа
 	// ввод только цифор
 	in3 := newNumericalEntry()
+	in3.Entry.TextStyle.Monospace = true
 
 	box := container.NewVBox(widget.NewLabel("Однострочный:"), in1,
 		widget.NewLabel("Многострочный: "), in2,
