c1f1c09cf92a3b6871fc627bdbb4113bffef9d6e
annettalekto <annanesterova248@gmail.com>
2023-08-19 08:03:46 +0300

    + goal type

--- miniprogs/todolist/main.go
+++ miniprogs/todolist/main.go
@@ -28,27 +28,43 @@ func mainForm() *fyne.Container {
 	// bar.SetValue(10)
 	// box := container.NewVBox(l1, bar)
 
-	pbar1 := CreateGoal("Читать ITM", 300)
-	pbar2 := CreateGoal("Читать ENG", 1300)
-	pbar3 := CreateGoal("Перебрать тетради", 15)
+	var goal1, goal2, goal3 goalType
+	goal1.Create("Читать ITM", 300)
+	goal2.Create("Читать ENG", 1300)
+	goal3.Create("Перебрать тетради", 15)
 
-	barBox := container.NewVBox(pbar1, pbar2, pbar3)
+	barBox := container.NewVBox(goal1.box, goal2.box, goal3.box)
 
 	split := container.NewHSplit(barBox, l2)
 	return container.NewBorder(nil, nil, nil, nil, split)
 }
 
-func CreateGoal(name string, max float64) *fyne.Container {
-	label := widget.NewLabel(name)
+// goalType data
+type goalType struct {
+	name        string
+	max         float64
+	value       float64
+	progressBar *widget.ProgressBar
+	button      *widget.Button
+	box         *fyne.Container
+}
 
-	pbar := widget.NewProgressBar()
-	pbar.Max = max
-	pbar.Min = 1
-	pbar.SetValue(0)
+// Create for goalType's progressBar
+func (g *goalType) Create(name string, max float64) {
+	g.name = name
+	g.max = max
+	g.value = 0
 
-	addProgress := widget.NewButton("Прогресс", nil)
+	label := widget.NewLabel(g.name)
 
-	boxH := container.NewBorder(nil, nil, nil, addProgress, pbar)
+	g.progressBar = widget.NewProgressBar()
+	g.progressBar.Max = g.max
+	g.progressBar.Min = 1
+	g.progressBar.SetValue(0)
 
-	return container.NewVBox(label, boxH)
+	g.button = widget.NewButton("Прогресс", nil)
+
+	boxH := container.NewBorder(nil, nil, nil, g.button, g.progressBar)
+
+	g.box = container.NewVBox(label, boxH)
 }
