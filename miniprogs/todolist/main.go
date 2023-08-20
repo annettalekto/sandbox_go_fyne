5be76ffa6c86c685611ad1b670fb53dca075fed5
annettalekto <annanesterova248@gmail.com>
2023-08-20 09:55:31 +0300

    + check

--- miniprogs/todolist/main.go
+++ miniprogs/todolist/main.go
@@ -19,25 +19,26 @@ func main() {
 }
 
 func mainForm() *fyne.Container {
-	// l1 := widget.NewLabel("Objects")
-	l2 := widget.NewLabel("buttons")
-
-	// bar := widget.NewProgressBar()
-	// bar.Max = 100
-	// bar.Min = 1
-	// bar.SetValue(10)
-	// box := container.NewVBox(l1, bar)
 
 	var goal1, goal2, goal3 goalType
-	goal1.Create("Читать ITM", 300)
-	goal2.Create("Читать ENG", 1300)
-	goal3.Create("Перебрать тетради", 15)
+	goal1.Create("Читать ITM:", 300)
+	goal2.Create("Читать ENG:", 1300)
+	goal3.Create("Перебрать тетради:", 15)
 
-	barBox := container.NewVBox(goal1.box, goal2.box, goal3.box)
+	check1 := CreateCheck("Уборка")
+	check2 := CreateCheck("Йога")
+	barBox := container.NewVBox(goal1.box, goal2.box, goal3.box, check1, check2)
 
+	l2 := widget.NewLabel("buttons")
 	split := container.NewHSplit(barBox, l2)
 	return container.NewBorder(nil, nil, nil, nil, split)
 }
+
+// ----------------------------------------------------------------------------
+// 										goal
+// ----------------------------------------------------------------------------
+
+// var goalSlice []goalType
 
 // goalType data
 type goalType struct {
@@ -62,9 +63,33 @@ func (g *goalType) Create(name string, max float64) {
 	g.progressBar.Min = 1
 	g.progressBar.SetValue(0)
 
-	g.button = widget.NewButton("Прогресс", nil)
+	g.button = widget.NewButton("Изменить", nil)
 
 	boxH := container.NewBorder(nil, nil, nil, g.button, g.progressBar)
 
 	g.box = container.NewVBox(label, boxH)
 }
+
+// ----------------------------------------------------------------------------
+// 										todo
+// ----------------------------------------------------------------------------
+// var todoSlice []goalType
+
+// todoType data
+// type goalType struct {
+// 	name        string
+// 	max         float64
+// 	value       float64
+// 	progressBar *widget.ProgressBar
+// 	button      *widget.Button
+// 	box         *fyne.Container
+// }
+
+func CreateCheck(name string) *widget.Check {
+	check := widget.NewCheck(name, nil)
+	return check
+}
+
+// ----------------------------------------------------------------------------
+// 										notes
+// ----------------------------------------------------------------------------
